ae25dbcf162bde4c4efd9d618aeb01664663d77f
anapillati239 <anapillati30@gmail.com>
2024-12-06 20:26:35 -0300

    Adiciona lógica em JavaScript para tornar a interface interativa

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('addTaskButton').addEventListener('click', () => {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+  
+    if (taskInput.value.trim() !== '') {
+      const newTask = document.createElement('li');
+      newTask.textContent = taskInput.value;
+      taskList.appendChild(newTask);
+      taskInput.value = '';
+    }
+  });
